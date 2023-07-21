53fa3a12518cda30641dcbce4b4f03d04d400bd5
Billy Jackson [Personal] <williamrjackson@gmail.com>
2023-07-21 16:21:45 -0400

    get json string

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 try {
-  const jsonPath = core.getInput('package-json');
-  const time = (new Date()).toTimeString();
+  const jsonPath = core.getInput('path');
+  const targetVal = core.getInput('value');
   var fs = require('fs')
   fs.readFile(jsonPath, 'utf8', function (err,data) {
     if (err) {
@@ -11,13 +10,8 @@ try {
     } else {
         let jsonData = JSON.parse(data);
 
-        let ver = jsonData["version"];
-        var nums = ver.replace(' ', '').split('.')
-        var revision = parseInt(nums[nums.length -1]);
-        nums[nums.length -1] = revision + 1;
-                
-        console.log(nums[nums.length -1])
-        core.setOutput("new-version", nums[0] + "." + nums[1] + "." + nums[2]);
+        let ver = jsonData[targetVal];
+        core.setOutput("value", ver);
     }
 });
 } catch (error) {
