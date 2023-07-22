d5e46b20969ef4cb4a2122db4c1181a6da217c6d
Billy Jackson [Personal] <williamrjackson@gmail.com>
2023-07-22 19:44:01 -0400

    check output of git tag

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ try {
             // once the command has completed, the callback function is called
             if (err) {
                 // log and return if we encounter an error
-                console.error("could not execute command: ", err)
-                return
+                console.error("could not execute command: ", err);
+                return;
             }
             // log the output received from the command
-            console.log("Output: \n", output)
+            console.log(output);
         })
       
         let jsonData = JSON.parse(data);
