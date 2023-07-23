de0fa371c654a8545bbcf0882a3e0cbd20bd6219
Billy Jackson [Personal] <williamrjackson@gmail.com>
2023-07-23 01:18:17 -0400

    x

--- index.js
+++ index.js
@@ -3,32 +3,26 @@ const { context, getOctokit } = require('@actions/github');
 
 async function run() {
   var fs = require('fs').promises;
-  try {
-    const jsonPath = core.getInput('path');
-    const inc = core.getInput('increment');
-    let maj = 0;
-    let min = 0;
-    let pch = 0;
-    var ver = "";
-    await fs.readFile(jsonPath, 'utf8', function (err, data) {
-      if (err) {
-        core.setFailed(err);
-      } else {
-        let jsonData = JSON.parse(data);
-        ver = jsonData['version'];
+  const jsonPath = core.getInput('path');
+  const inc = core.getInput('increment');
+  var jsonData;
+  await fs.readFile(jsonPath, 'utf8', function (err, data) {
+    if (err) {
+      core.setFailed(err);
+    } else {
+      jsonData = JSON.parse(data);
+    }
+  })
+  
+  let ver = jsonData['version'];
 
-
-        const nums = ver.split('.');
-        maj = parseInt(nums[0]);
-        min = parseInt(nums[1]);
-        pch = parseInt(nums[2]);
-      }
-    })
-  } catch (error) {
-    core.setFailed(error);
-  }
   var tagExists = await checkTag(ver);
   if (tagExists) {
+    const nums = ver.split('.');
+    let maj = parseInt(nums[0]);
+    let min = parseInt(nums[1]);
+    let pch = parseInt(nums[2]);
+
     switch (inc) {
       case "major":
         maj++;
