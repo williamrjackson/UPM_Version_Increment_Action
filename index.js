fecd38c75a2977f753cac0ff1c008e296120fc33
Billy Jackson [Personal] <williamrjackson@gmail.com>
2023-07-23 01:05:38 -0400

    x

--- index.js
+++ index.js
@@ -1,66 +1,70 @@
 const core = require('@actions/core');
 const { context, getOctokit } = require('@actions/github');
 
-function run() {
+async function run() {
+  var fs = require('fs')
   try {
     const jsonPath = core.getInput('path');
     const inc = core.getInput('increment');
-    var fs = require('fs')
+    let maj = 0;
+    let min = 0;
+    let pch = 0;
+    var ver = "";
     fs.readFile(jsonPath, 'utf8', function (err, data) {
       if (err) {
-        console.log(err);
+        core.setFailed(err);
       } else {
         let jsonData = JSON.parse(data);
-        let ver = jsonData['version'];
+        ver = jsonData['version'];
 
 
         const nums = ver.split('.');
-        let maj = parseInt(nums[0]);
-        let min = parseInt(nums[1]);
-        let pch = parseInt(nums[2]);
+        maj = parseInt(nums[0]);
+        min = parseInt(nums[1]);
+        pch = parseInt(nums[2]);
+      }
+    })
+  } catch (error) {
+    core.setFailed(error);
+  }
+  var tagExists = await checkTag(`v${ver}`);
+  if (tagExists) {
+    switch (inc) {
+      case "major":
+        maj++;
+        break;
+      case "minor":
+        min++;
+        break;
+      default:
+        pch++;
+        break;
+    }
 
-        if (checkTag(`v${ver}`)) {
-          switch (inc) {
-            case "major":
-              maj++;
-              break;
-            case "minor":
-              min++;
-              break;
-            default:
-              pch++;
-              break;
-          }
-
-          ver = maj + "." + min + "." + pch
-          jsonData['version'] = newVer;
-          // Stringify and write to file.
-          fs.writeFile(jsonPath, JSON.stringify(jsonData), function (err) {
-            if (err) {
-              console.log(err);
-            }
-          });
-        }
-        core.setOutput("value", ver);
+    ver = maj + "." + min + "." + pch
+    jsonData['version'] = newVer;
+    // Stringify and write to file.
+    fs.writeFile(jsonPath, JSON.stringify(jsonData), function (err) {
+      if (err) {
+        core.setFailed(err);
       }
     });
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  };
+  core.setOutput("value", ver);
 }
 
-function checkTag(tag) {
+async function checkTag(tag) {
   console.log(`Searching for tag: ${tag}`);
-  const token = core.getInput( 'github_token' );
-  if ( ! token ) {
-    setFailed( 'Input `github_token` is required' );
+  const token = core.getInput('github_token');
+  if (!token) {
+    setFailed('Input `github_token` is required');
     return;
   }
   // Get owner and repo from context of payload that triggered the action
   const { owner, repo } = context.repo
   const octokit = getOctokit(token)
   try {
-    const getRefResponse = octokit.rest.git.getRef({
+    const getRefResponse = await octokit.rest.git.getRef({
       owner,
       repo,
       ref: `tags/${tag}`
