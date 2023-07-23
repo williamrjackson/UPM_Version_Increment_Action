a006a3c9a61e3b91ec54bdf9595affb160a921d2
Billy Jackson [Personal] <williamrjackson@gmail.com>
2023-07-23 02:05:32 -0400

    x

--- index.js
+++ index.js
@@ -2,69 +2,59 @@ const core = require('@actions/core');
 const { context, getOctokit } = require('@actions/github');
 
 async function run() {
-  const inc = core.getInput('increment');
-  
-  let ver = await Promise(getVersion());
-
-  var tagExists = await Promise(checkTag(ver));
-  if (tagExists) {
-    console.log("tag exists");
-    const nums = ver.split('.');
-    let maj = parseInt(nums[0]);
-    let min = parseInt(nums[1]);
-    let pch = parseInt(nums[2]);
-
-    switch (inc) {
-      case "major":
-        maj++;
-        break;
-      case "minor":
-        min++;
-        break;
-      default:
-        pch++;
-        break;
-    }
-
-    ver = maj + "." + min + "." + pch
-  };
-  core.setOutput("value", ver);
-}
-async function getVersion() {
-  return new Promise(resolve => setTimeout(() => {
-    console.log(`getting version`);
-    var fs = require('fs').promises;
+  var fs = require('fs')
+  try {
     const jsonPath = core.getInput('path');
+    const inc = core.getInput('increment');
+    let maj = 0;
+    let min = 0;
+    let pch = 0;
+    var ver = "";
+    var newVer = "";
     fs.readFile(jsonPath, 'utf8', function (err, data) {
       if (err) {
         core.setFailed(err);
       } else {
-        jsonData = JSON.parse(data);
-        var ver = jsonData['version'];
-        console.log(`returning ${ver}`);
-        resolve({data: ver});
-      }
-  })}, 5000));
-}
+        let jsonData = JSON.parse(data);
+        ver = jsonData['version'];
 
-async function writeVersion(version)
-{
-  console.log(`writing: ${version}`);
-  var fs = require('fs').promises;
-  const jsonPath = core.getInput('path');
-  await fs.readFile(jsonPath, 'utf8', function (err, data) {
-    if (err) {
-      core.setFailed(err);
-    } else {
-      jsonData = JSON.parse(data);
-      jsonData['version'] = version;
+        const nums = ver.split('.');
+        maj = parseInt(nums[0]);
+        min = parseInt(nums[1]);
+        pch = parseInt(nums[2]);
+      }
+      switch (inc) {
+        case "major":
+          maj++;
+          break;
+        case "minor":
+          min++;
+          break;
+        default:
+          pch++;
+          break;
+      }
+      newVer = maj + "." + min + "." + pch
+    });
+    var tagExists = await checkTag(`v${ver}`);
+    if (tagExists)
+    {
+      jsonData['version'] = newVer;
+      // Stringify and write to file.
       fs.writeFile(jsonPath, JSON.stringify(jsonData), function (err) {
         if (err) {
           core.setFailed(err);
         }
       });
+      core.setOutput("value", newVer);
     }
-  })
+    else
+    {
+      core.setOutput("value", ver);
+    }
+  } catch (error) {
+    core.setFailed(error);
+  }
 }
 
 async function checkTag(tag) {
